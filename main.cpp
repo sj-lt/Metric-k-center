faea55347ebce9f3b18db2cfff98801f89a531b3
Stanisław Lutkiewicz <stas.lutkiewicz@gmail.com>
2019-10-23 12:25:43 +0200

    loading from command line + testing set

--- main.cpp
+++ main.cpp
@@ -4,17 +4,15 @@ int main(int argc, char **argv)
 {
 	using namespace std;
 
-	solution_t experiment({{"Warszawa", 52.2330269, 20.7810081},
-						   {"Gdansk", 54.3612063, 18.5499431},
-						   {"Bydgoszcz", 53.1169002, 17.9008963},
-						   {"Poznan", 52.456009, 16.896973},
-						   {"Wrocław", 51.1000000, 17.0333300},
-						   {"Kraków", 50.0614300, 19.9365800},
-						   {"Lublin", 51.2500000, 22.5666700}},
-						  WAREHOUSES);
 
-	ifstream is("citiesTest6Csv.json"); // open file
-	is >> experiment;
+	solution_t experiment;
+	if (argc > 2) {
+		std::ifstream is(argv[1]); // open file
+		is >> experiment;
+	} else {
+		cin >> experiment;
+	}
+
 
 	experiment.numberOfWarehouses = WAREHOUSES;
 	chaseSequence combinations = chaseSequence(experiment);
@@ -23,8 +21,19 @@ int main(int argc, char **argv)
 	cout << "duration: " << duration << endl;
 	cout << "best score: " << combinations.problem_.getBestScoreKm() << endl;
 	cout<<"number of cities"<<combinations.problem_.problem->cities.size()<<endl;
-	ofstream os("magazyny1.json"); // open file
-	os << combinations.problem_;
+
+
+	//Save or cout
+	solution_t result;
+	result = combinations.problem_;
+	if (argc > 2) {
+		std::ofstream os(argv[2]); // open file
+		os << result;
+	} else {
+		cout << result << endl;
+	}
+
+
 	visualize(&combinations.problem_);
   
 };
