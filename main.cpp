9c2cd8719ea502e8dbaba3bd283acc8df01f9d7b
slutkiewicz <33038766+slutkiewicz@users.noreply.github.com>
2019-10-23 14:35:38 +0200

    Update main.cpp
    solution_t fix

--- main.cpp
+++ main.cpp
@@ -5,15 +5,23 @@ int main(int argc, char **argv)
 	using namespace std;
 
 
-	solution_t experiment;
+	solution_t experiment({{"Warszawa", 52.2330269, 20.7810081},
+						   {"Gdansk", 54.3612063, 18.5499431},
+						   {"Bydgoszcz", 53.1169002, 17.9008963},
+						   {"Poznan", 52.456009, 16.896973},
+						   {"Wrocław", 51.1000000, 17.0333300},
+						   {"Kraków", 50.0614300, 19.9365800},
+						   {"Lublin", 51.2500000, 22.5666700}},
+						  WAREHOUSES);
+
 	if (argc > 2) {
-		std::ifstream is(argv[1]); // open file
+		ifstream is(argv[1]); // open file
 		is >> experiment;
 	} else {
 		cin >> experiment;
 	}
 
-
+	
 	experiment.numberOfWarehouses = WAREHOUSES;
 	chaseSequence combinations = chaseSequence(experiment);
 	auto duration = combinations.mesureTime();
@@ -27,7 +35,7 @@ int main(int argc, char **argv)
 	solution_t result;
 	result = combinations.problem_;
 	if (argc > 2) {
-		std::ofstream os(argv[2]); // open file
+		ofstream os(argv[2]); // open file
 		os << result;
 	} else {
 		cout << result << endl;
@@ -61,4 +69,5 @@ void visualize(solution_t *problem){
   htmlout << html_footer;
   htmlout.close();
 };
+
 
